9f360b3509fa023e586c73a928ffc9f62e4a6352
Pradyumnsinghrathore <119104469+Pradyumnsinghrathore@users.noreply.github.com>
2022-11-26 15:23:48 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -1,61 +1,19 @@
-#!/bin/bash
-
-# Variables
-name="shankith"
-s3_bucket="upgrad-shankith"
-
-# update the ubuntu repositories
-apt update -y
-
-# Check if apache2 is installed
-if [[ apache2 != $(dpkg --get-selections apache2 | awk '{print $1}') ]]; then
-	#statements
-	apt install apache2 -y
-fi
-
-# Ensures that apache2 service is running
-running=$(systemctl status apache2 | grep active | awk '{print $3}' | tr -d '()')
-if [[ running != ${running} ]]; then
-	#statements
-	systemctl start apache2
-fi
-
-# Ensures apache2 Service is enabled 
-enabled=$(systemctl is-enabled apache2 | grep "enabled")
-if [[ enabled != ${enabled} ]]; then
-	#statements
-	systemctl enable apache2
-fi
-
-# Creating file name
+sudo apt update -y
+echo "packages updated successfully"
+apache2 --version
+apt-get install apache2
+echo "apache2 installed successfully"
+systemctl list-units --type=service --state=running
+echo "apache2 server is enabled and running"
+name=$'pradyumn'
 timestamp=$(date '+%d%m%Y-%H%M%S')
-
-# Create tar archive of apache2 access and error logs
-cd /var/log/apache2
-tar -cf /tmp/${name}-httpd-logs-${timestamp}.tar *.log
-
-# copy logs to s3 bucket
-if [[ -f /tmp/${name}-httpd-logs-${timestamp}.tar ]]; then
-	#statements
-	aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
-fi
-
-docroot="/var/www/html"
-# Check if inventory file exists
-if [[ ! -f ${docroot}/inventory.html ]]; then
-	#statements
-	echo -e 'Log Type\t-\tTime Created\t-\tType\t-\tSize' > ${docroot}/inventory.html
-fi
-
-# Inserting Logs into the file
-if [[ -f ${docroot}/inventory.html ]]; then
-	#statements
-    size=$(du -h /tmp/${name}-httpd-logs-${timestamp}.tar | awk '{print $1}')
-	echo -e "httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${docroot}/inventory.html
-fi
-
-# Create a cron job that runs service every minutes/day
-if [[ ! -f /etc/cron.d/automation ]]; then
-	#statements
-	echo "* * * * * root /root/automation.sh" >> /etc/cron.d/automation
-fi
+tar cvf- $name-httpd-logs-$timestamp.tar /var/log/apache2/access.log /var/log/apache2/error.log
+echo "tar file created"
+mv $name-httpd-logs-$timestamp.tar /tmp/$name-httpd-logs-$timestamp.tar
+echo "tar moved to /tmp/ directory"
+sudo apt update
+sudo apt install awscli
+echo "awscli installed"
+aws s3 \
+cp /tmp/$name-httpd-logs-$timestamp.tar \
+s3://upgrad-pradyumn/$name-httpd-logs-$timestamp.tar
