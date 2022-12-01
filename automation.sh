6e9d0cceff9fd89a83f7c2b050e3d6a44def922d
Pradyumnsinghrathore <119104469+Pradyumnsinghrathore@users.noreply.github.com>
2022-12-01 18:56:29 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -17,3 +17,25 @@ echo "awscli installed"
 aws s3 \
 cp /tmp/$name-httpd-logs-$timestamp.tar \
 s3://upgrad-pradyumn/$name-httpd-logs-$timestamp.tar
+
+fi
+
+docroot="/var/www/html"
+# Check if inventory file exists
+if [[ ! -f ${docroot}/inventory.html ]]; then
+	#statements
+	echo -e 'Log Type\t-\tTime Created\t-\tType\t-\tSize' > ${docroot}/inventory.html
+fi
+
+# Inserting Logs into the file
+if [[ -f ${docroot}/inventory.html ]]; then
+	#statements
+    size=$(du -h /tmp/${name}-httpd-logs-${timestamp}.tar | awk '{print $1}')
+	echo -e "httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${docroot}/inventory.html
+fi
+
+# Create a cron job that runs service every minutes/day
+if [[ ! -f /etc/cron.d/automation ]]; then
+	#statements
+	echo "* * * * * root /root/automation.sh" >> /etc/cron.d/automation
+fi
